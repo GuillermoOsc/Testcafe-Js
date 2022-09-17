9901282c4ce75ededd607fdeb851c61c96df18bf
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-09-17 01:53:17 -0300

    Implementación de variables de entorno para datos sensibles

--- pom/tests/loginTest.js
+++ pom/tests/loginTest.js
@@ -2,10 +2,14 @@
 
 import loginPage from '../pages/loginPage';
 import inventoryPage from '../pages/inventoryPage'
+import dotenv from 'dotenv'
+
+dotenv.config() // Se importa la configuración para procesamiento de las variables de ambiente.
 
 fixture `Login test suite`
-    .page `https://www.saucedemo.com/`
+    .page`${process.env.URL_TEST}` // Plantillas literales
+
 test('El usuario debe iniciar sesión correctamente', async t => {
-    await loginPage.submitLoginForm('standard_user', 'secret_sauce')
+    await loginPage.submitLoginForm(process.env.USER_TEST,process.env.PASS_TEST)
     await t.expect(inventoryPage.title.exists).ok()
 } )
