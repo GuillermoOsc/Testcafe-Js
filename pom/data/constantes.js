6f195155a9405a1fbf3d0fdca128470dce84941a
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-09-22 02:19:30 -0300

    Implementación de proveedor de datos "Data providers"

---
+++ pom/data/constantes.js
@@ -0,0 +1,35 @@
+// Data provider: Proveedor de datos: Implementación de data provider, practica para optimizar los datos
+// generando un archivo aislado con las constantes y sus respectivos datos
+// El propósito de los Data Providers es obtener información jerárquica para
+//que quien la necesita pueda luego hacer algo con ella.
+// Buscar más info sobre Data providers
+
+
+import dotenv from 'dotenv'
+dotenv.config()
+// Se importa la configuración para procesamiento de las variables de ambiente.
+
+
+export const URL = {
+    URL_BASE_TEST: process.env.URL_TEST
+
+}
+
+export const CREDENTIALS = {
+    VALID_CREDENTIALS:{
+        USER_BASE_TEST: process.env.USER_TEST,
+        PASS_BASE_TEST:process.env.PASS_TEST
+    },
+    INVALID_CREDENTIALS: {
+        INVALID_USER_TEST: 'Invalid user',
+        INVALID_PASS_TEST: 'Invalid pass'
+    }  
+}
+
+
+export const MESSAGES = {
+    INVALID_USER_AND_PASS:'Epic sadface: Username and password do not match any user in this service',
+    NULL_USER:'Epic sadface: Username is required',
+    NULL_PASS:'Epic sadface: Password is required',
+    NULL_USER_AND_NULL_PASS: 'Epic sadface: Username is required'
+}
--- pom/tests/loginTest.js
+++ pom/tests/loginTest.js
@@ -2,14 +2,13 @@
 
 import loginPage from '../pages/loginPage';
 import inventoryPage from '../pages/inventoryPage'
-import dotenv from 'dotenv'
+import { URL, CREDENTIALS } from '../data/constantes'
 
-dotenv.config() // Se importa la configuración para procesamiento de las variables de ambiente.
 
 fixture `Login test suite`
-    .page`${process.env.URL_TEST}` // Plantillas literales
+    .page`${URL.URL_BASE_TEST}` // Plantillas literales
 
 test('El usuario debe iniciar sesión correctamente', async t => {
-    await loginPage.submitLoginForm(process.env.USER_TEST,process.env.PASS_TEST)
+    await loginPage.submitLoginForm(CREDENTIALS.VALID_CREDENTIALS.USER_BASE_TEST,CREDENTIALS.VALID_CREDENTIALS.PASS_BASE_TEST)
     await t.expect(inventoryPage.title.exists).ok()
 } )
